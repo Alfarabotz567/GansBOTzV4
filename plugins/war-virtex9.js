cf00fca1da6adc11a37df0071d2a3d01217d1da5
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-09-25 10:22:37 +0700

    Update war-virtex9.js

--- plugins/war-virtex9.js
+++ plugins/war-virtex9.js
@@ -14,11 +14,11 @@ m.reply(anu)
 handler.help = ['virtex9']
 handler.tags = ['war']
 handler.command = /^(virtex9)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
-handler.private = true
+handler.private = false
 
 handler.admin = false
 handler.botAdmin = false
