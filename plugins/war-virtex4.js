d0c0bd0d087f866b88e3b0b8e0f3d2e633041425
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-09-25 10:18:03 +0700

    Update war-virtex4.js

--- plugins/war-virtex4.js
+++ plugins/war-virtex4.js
@@ -58,11 +58,11 @@ m.reply(anu)
 handler.help = ['virtex4']
 handler.tags = ['war']
 handler.command = /^(virtex4)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
-handler.private = true
+handler.private = false
 
 handler.admin = false
 handler.botAdmin = false
